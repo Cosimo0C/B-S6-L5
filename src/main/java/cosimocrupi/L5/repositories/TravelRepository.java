7df2f0a63966ca176ab2d8e0b904254980981149
Cosimo0C <cosimocrupi01@gmail.com>
2025-07-28 17:35:00 +0200

    finished

--- src/main/java/cosimocrupi/L5/repositories/TravelRepository.java
+++ src/main/java/cosimocrupi/L5/repositories/TravelRepository.java
@@ -1,15 +1,14 @@
 package cosimocrupi.L5.repositories;
 
-import cosimocrupi.L5.entities.Reservation;
 import cosimocrupi.L5.entities.Travel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface TravelRepository extends JpaRepository<Travel, UUID> {
-    List<Travel> findByDestination(String destination);
+    Optional<Travel> findByReservationId(String reservationId);
 
 }
